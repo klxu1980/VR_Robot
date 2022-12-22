9ff742991f77a3231b5cbc75d1d7b91ebf578b57
Kalahari <kailiang1980@gmail.com>
2022-12-22 20:08:29 +0800

    python共享内存程序

---
+++ Python/SharedMem.py
@@ -0,0 +1,70 @@
+import mmap
+import numpy as np
+import cv2
+import time
+from namedmutex import NamedMutex
+
+
+class SharedMemory(object):
+    def __init__(self, mem_name, mem_size, mem_type):
+        self.mem_name = mem_name
+        self.mem_size = mem_size
+        self.shared_mem = mmap.mmap(fileno=0, length=mem_size, tagname=mem_name, access=mem_type)
+        self.mutex = NamedMutex(mem_name + "_mutex")
+
+    def __del__(self):
+        self.shared_mem.close()
+
+    def send(self, data):
+        self.shared_mem.write(data)
+
+    def read(self, byte_cnt):
+        return self.shared_mem.read(byte_cnt)
+
+    def send_images(self, images):
+        """
+        Send a list of images(raw BGR format) through shared memory.
+        :param images: Image list
+        :return:
+        """
+        self.mutex.acquire()
+        self.shared_mem.seek(0)
+        for img in images:
+            img_size = np.array(img.shape, dtype=np.int32)
+            self.shared_mem.write(img_size.tobytes())
+            self.shared_mem.write(img.tobytes())
+        empty = np.zeros(3, dtype=np.int32)
+        self.shared_mem.write(empty.tobytes())
+        self.mutex.release()
+
+    def read_images(self):
+        """
+        Read a list of images(raw BGR format) from the shared memory.
+        :return:
+        """
+        self.mutex.acquire()
+        self.shared_mem.seek(0)
+        img_list = list()
+        while True:
+            img_shape = self.shared_mem.read(12)
+            img_shape = np.frombuffer(img_shape, dtype=np.int32)
+            img_size = img_shape[0] * img_shape[1] * img_shape[2]
+            if not img_size:
+                break
+            img_data = self.shared_mem.read(img_size)
+            img = np.frombuffer(img_data, dtype=np.uint8).reshape(img_shape)
+            img_list.append(img)
+        self.mutex.release()
+        return img_list
+
+
+if __name__ == '__main__':
+    img1 = cv2.imread("lena.jpg")
+    img2 = cv2.imread("building.jpg")
+    img = (img1, img2)
+
+    shmm = SharedMemory(mem_name="shared_memory1", mem_size=1024*1024*3*2, mem_type=mmap.ACCESS_WRITE)
+    img_idx = 0
+    while True:
+        shmm.send_images(img)
+        time.sleep(0.05)
